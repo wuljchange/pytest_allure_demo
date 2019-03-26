d7f7964d9e0c3dfef2c6d0685a45ae4a9648b465
WULINJIANG1 <wulinjiang1@kingsoft.com>
2019-03-26 15:00:43 +0800

    [feat] 添加注释

--- test/run.py
+++ test/run.py
@@ -4,35 +4,31 @@ import allure
 from py_case.data import DATA, DATA2, DATA3
 
 
-@allure.feature('test_calculate_number')
+# 标记功能模块的关键字 calculate
 @pytest.mark.calculate
 class TestCalculate:
+    # feature表示主要功能模块
+    @allure.feature('test_calculate_number')
+    # story表示主要功能模块下的分支功能
     @allure.story('test_add')
-    @allure.severity('blocker')
-    @allure.issue("http://www.baidu.com")
-    @allure.testcase("http://www.jira.com")
+    # 参数化case
     @pytest.mark.parametrize('cid, num1, num2, excepted', DATA)
     def test_add(self, calculate, cid, num1, num2, excepted):
         real = calculate.add(self, num1, num2)
         assert real == excepted
 
+    @allure.feature('test_calculate_number')
     @allure.story('test_sub')
-    @allure.severity('blocker')
-    @allure.issue("http://www.baidu.com")
-    @allure.testcase("http://www.jira.com")
     @pytest.mark.parametrize('cid, num1, num2, excepted', DATA2)
     def test_sub(self, calculate, cid, num1, num2, excepted):
         real = calculate.sub(self, num1, num2)
         assert real == excepted
 
 
-@allure.feature('test_replace_string')
 @pytest.mark.replace
 class TestReplace:
+    @allure.feature('test_replace_string')
     @allure.story('test_replace_all')
-    @allure.severity('blocker')
-    @allure.issue("http://www.baidu.com")
-    @allure.testcase("http://www.jira.com")
     @pytest.mark.parametrize('cid, raw, pattern, new, excepted', DATA3)
     def test_replace(self, replace, cid, raw, pattern, new, excepted):
         real = replace.replace_all(self, raw, pattern, new)
